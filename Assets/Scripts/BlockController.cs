c56003936fde35fda6ab4e17e1512ceae678570e
PixelScream <underscoredickie@gmail.com>
2014-12-14 21:12:35 +0000

    commit before graphic overhaul

--- Assets/Scripts/BlockController.cs
+++ Assets/Scripts/BlockController.cs
@@ -2,9 +2,17 @@
 using System.Collections;
 
 public class BlockController : MonoBehaviour {
+	private Vector2 dimensions;
+	void Start() {
+		dimensions = transform.GetComponent<BoxCollider2D> ().size;
+	}
 	
 	public void BreakBlock() {
 		//transform.collider2D.enabled = false;
 		Destroy (gameObject);
+	}
+
+	public void CheckSides() {
+
 	}
 }
